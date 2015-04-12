146959b77c8b9dbed2e5242c6cf30f9efe8e7878
David Fowler <davidfowl@gmail.com>
2015-04-11 18:48:21 -0700

    Made a change to make mono 3.12.x work

--- src/Microsoft.AspNet.Cryptography.Internal/Properties/AssemblyInfo.cs
+++ src/Microsoft.AspNet.Cryptography.Internal/Properties/AssemblyInfo.cs
@@ -6,8 +6,10 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+#if !MONO
 // we only ever p/invoke into DLLs known to be in the System32 folder
 [assembly: DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+#endif
 
 [assembly: InternalsVisibleTo("Microsoft.AspNet.Cryptography.Internal.Test")]
 [assembly: InternalsVisibleTo("Microsoft.AspNet.Cryptography.KeyDerivation")]
